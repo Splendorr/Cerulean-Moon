63926c3562087c4892f2e0b36d522c213f533af9
Splendorr <simolinic@gmail.com>
2015-06-06 17:54:04 -0400

    Separated JS vars into their own file. Simplified the code for creating GFYCATs so that the variable array is just the IDs. Various other small changes.

--- assets/js/cm.js
+++ assets/js/cm.js
@@ -1,23 +1,3 @@
-// Hey nacho!
-// These are the variable declarations; these lists instantiate the variables below, and should match:
-var youTubeVideoID, gfySingles;
-
-// This is the YouTube Video ID, just the part of URL after https://www.youtube.com/watch?v=
-var youTubeVideoID = 'sdh2VsCD9-Q';
-
-// And these are the GFYCAT IDs.
-// If you change the total number here, you need to adjust the slides in index.html to match. You can see that #gfySingle0 and #gfySingle0triple, etc. correspond to the indices of this array.
-var gfySingles = [
-  'FragrantHeartyKawala', // Slide 0
-  'FragrantHeartyKawala', // Slide 1
-  'FragrantHeartyKawala', // Slide 2
-  'FragrantHeartyKawala', // Slide 3
-  'FragrantHeartyKawala', // Slide 4
-  'FragrantHeartyKawala', // Slide 5
-]
-
-
-
 $(document).ready(function(){
 
   // SLICK SLIDER
