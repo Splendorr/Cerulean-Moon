3aa33c39718e39746416a309a50272c3e9391b5b
Splendorr <simolinic@gmail.com>
2015-06-14 13:31:29 -0400

    About to try switching to just one slide on large screens.

--- assets/js/vars.js
+++ assets/js/vars.js
@@ -9,13 +9,19 @@ var youTubeVideoID = 'sdh2VsCD9-Q';
 // If you change the total number here, you need to adjust the slides in index.html to match. You can see that #gfySingle0 and #gfySingle0triple, etc. correspond to the indices of this array.
 
 var gfySingles = [
-  'FragrantHeartyKawala', // Slide 0
-  'FragrantHeartyKawala', // Slide 1
-  'FragrantHeartyKawala', // Slide 2
-  'FragrantHeartyKawala', // Slide 3
-  'FragrantHeartyKawala', // Slide 4
-  'FragrantHeartyKawala', // Slide 5
-]
+  'VillainousUnrealisticBallpython', // Slide 0
+  'AcclaimedLiquidIbizanhound', // Slide 1
+  'PointlessCheerfulBurro', // Slide 2
+  'SingleBoldAnemoneshrimp', // Slide 3
+  'WateryOblongCoelacanth', // Slide 4
+  'InconsequentialDifferentKite', // Slide 5
+  // 'DeficientImpureAnkole', // Slide 6
+  // 'HopefulBeautifulHen', // Slide 7
+  // 'TameCavernousIzuthrush', // Slide 8
+  // 'VapidSerpentineHippopotamus', // Slide 9
+  // 'NegligibleRecentAfghanhound', // Slide  10
+  // 'FragrantHeartyKawala', // Slide  11
+];
 
 // This is the number of small-screen single-GFY slides, starting from 0. This should match the total array index of gfySingles above.
 var gfySingleMax = 5;
