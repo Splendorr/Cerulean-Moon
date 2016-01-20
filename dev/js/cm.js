29e4937109ac24d491fe7917552b0cee81783991
Splendorr <simolinic@gmail.com>
2016-01-20 00:30:10 -0500

    Updated YouTube JS API. Changed video tags to .widescreen.vimeo

--- dev/js/cm.js
+++ dev/js/cm.js
@@ -246,19 +246,19 @@ $(document).ready(function(){
 // YOU TUBE **********************************
 // Load the IFrame Player API code asynchronously.
 var tag = document.createElement('script');
-tag.src = "https://www.youtube.com/player_api";
+tag.src = "https://www.youtube.com/iframe_api";
 var firstScriptTag = document.getElementsByTagName('script')[0];
 firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 
 // Replace the 'ytplayer' element with an  and
 // YouTube player after the API code downloads.
 var player;
-function onYouTubePlayerAPIReady() {
+function onYouTubeIframeAPIReady() {
   player = new YT.Player('player', {
     height: '1080',
     width: '1920',
     videoId: youTubeVideoID,
-    playerVars: { 'autoplay': 1, 'vq': 'HD720', 'autohide': 1, 'controls': 1, 'loop': 1, 'rel': 0 },
+    playerVars: { 'autoplay': 1, 'vq': 'HD720', 'autohide': 1, 'controls': 1, 'loop': 1, 'modestbranding': 1, 'rel': 0, 'showinfo': 0 },
     events: {
       'onReady': onPlayerReady,
       'onStateChange': onPlayerStateChange
--- dev/js/vars.js
+++ dev/js/vars.js
@@ -3,7 +3,8 @@
 var youTubeVideoID, gfySingles;
 
 // This is the YouTube Video ID, just the part of URL after https://www.youtube.com/watch?v=
-var youTubeVideoID = '_UV-oh7x_CE';
+// var youTubeVideoID = '_UV-oh7x_CE';
+var youTubeVideoID = 'Pxqho7r7HEc';
 
 // And these are the GFYCAT IDs.
 // If you change the total number here, you need to adjust the slides in index.html to match. You can see that #gfySingle0 and #gfySingle0triple, etc. correspond to the indices of this array.
